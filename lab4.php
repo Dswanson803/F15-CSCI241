a035ed0582c92107982ef5c667949b1c02f409a5
Dylan Swanson <swansond3@winthrop.edu>
2015-10-22 13:06:26 -0400

    Fixes #1

--- lab4.php
+++ lab4.php
@@ -12,34 +12,44 @@
 				<h1>Times Table Generator</h1>
 				<form method="POST" action="lab4.php">
 				<table class="get-table">
-					<tr><td>Start Value: </td><td><input type="number" name="start"></td></tr>
-					<tr><td>End Value: </td><td><input type="number" name="end"></td></tr>
+					<tr><td>Start Value: </td><td><input type="text" name="start"></td></tr>
+					<tr><td>End Value: </td><td><input type="text" name="end"></td></tr>
 				</table>
 					<input type="submit" value="Submit" class="button">
 				</form>
 			<?php
 			} else if($_SERVER["REQUEST_METHOD"] == "POST") {
-				$end = (int) $_POST["end"];
-				$start = (int) $_POST["start"];
-				if (is_int($start) && is_int($end)) {
-					echo "<h1>Times Table Generator</h1>"; //page title
-					echo "<table class='post-table'>"; //table start
-					echo "<tr>";
-					echo "<td class='special'> </td>";
-					for ($x = $start; $x <= $end; $x++){ //header row
-						echo "<td class='special'>" . $x . "</td>";
-					}
-					echo "</tr>";
-					for ($x = $start; $x <= $end; $x++){ //header column
-						echo "<tr><td class='special'>" . $x . "</td>";
-						for ($y= $start; $y <= $end; $y++){ //table data
-							echo "<td>" . ($x * $y) . "</td>";
-						}
-						echo "</tr>";
-					}
-					echo "</table>";
+				if($_POST["start"] > $_POST["end"]){
+					$end = $_POST["start"];
+					$start = $_POST["end"];
+				} else {
+					$end = $_POST["end"];
+					$start = $_POST["start"];
 				}
-				else {
+				if(is_numeric($start) && is_numeric($end)) {
+					$test = (int) $start;
+					$test2 = (int) $end;
+					if($test == $start && $end == $test2){
+							echo "<h1>Times Table Generator</h1>"; //page title
+							echo "<table class='post-table'>"; //table start
+							echo "<tr>";
+							echo "<td class='special'> </td>";
+							for ($x = $start; $x <= $end; $x++){ //header row
+								echo "<td class='special'>" . $x . "</td>";
+							}
+							echo "</tr>";
+							for ($x = $start; $x <= $end; $x++){ //header column
+								echo "<tr><td class='special'>" . $x . "</td>";
+								for ($y= $start; $y <= $end; $y++){ //table data
+									echo "<td>" . ($x * $y) . "</td>";
+								}
+								echo "</tr>";
+							}
+							echo "</table>";
+					} else {
+						echo "Please enter valid integers.";
+					}
+				} else {
 					echo "Please enter valid integers.";
 				}
 			}
